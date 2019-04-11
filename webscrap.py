2335e35a0d986251bcf63b1385a8142651b9bc29
jmvsevilleja <jmvsevilleja@gmai.com>
2019-04-12 05:06:37 +0800

    windows

--- webscrap.py
+++ webscrap.py
@@ -1,17 +1,22 @@
 # Import Selenium webdrive
 from selenium import webdriver
 import pyautogui
+import platform
+
 # invoke chrome option to load extensions
 options = webdriver.ChromeOptions()
 
+if(platform.system() == 'Windows'):
+    driver = webdriver.Chrome('./chromedriver.exe')
+elif(platform.system() == 'Linux'):
+    driver = webdriver.Chrome('./chromedriver')
 
-driver = webdriver.Chrome('./chromedriver')
 driver.maximize_window()  # maximize the
 driver.get("https://www.google.com")
 # extension image is saved in project folder as findicon.png
 # locateOnScreen, a pyautogui method to locate the extension' x & y coordinates in screen
 # save the extension as image
-buttonx, buttony = pyautogui.locateCenterOnScreen('mic.png')
+buttonx, buttony = pyautogui.locateCenterOnScreen('mic2.png', grayscale=True)
 # print(pyautogui.locateCenterOnScreen('mic.png'))
 # trigger click event using the pyutogui click method
 pyautogui.click(buttonx, buttony)
